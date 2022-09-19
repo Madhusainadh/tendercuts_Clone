83e9413235b6cf8c0a8223515121269c84292dca
madhusainadh <lalammadhusainadh150@gmail.com>
2022-09-20 00:23:20 +0530

    firstAdd

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Component/Counter';
+import { useState } from 'react';
+import { Box, Button, Flex, SimpleGrid } from '@chakra-ui/react';
+import Todo from './Component/Todo';
+
+function App() {
+
+  const [t,sett] =useState(false)
+  return (
+    <div className="App">
+
+
+commit one
+    </div>
+  );
+}
+
+export default App;
