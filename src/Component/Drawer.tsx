91b1d27fa3f3848635575ce9e18126de48e0b34c
jstgrowup <deysubham999@gmail.com>
2022-09-26 23:38:11 +0530

    added

--- src/Component/Drawer.tsx
+++ src/Component/Drawer.tsx
@@ -1,44 +1,38 @@
-
 import {
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    Radio,
-    RadioGroup,
-    Stack,
-    Button,
-  } from '@chakra-ui/react'
-import React, { useState } from 'react'
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 export function PlacementExample() {
-    const [placement, setPlacement] = React.useState('top')
-    const [isOpen,setisOpen]=useState(false)
-  const onOpen=()=>{
-setisOpen(!isOpen)
-  }
-  const onClose=()=>{
-    setisOpen(!isOpen)
-  }
-    return (
-      <>
-        <Button colorScheme='blue' onClick={onOpen}>
-          Open
-        </Button>
-        <Drawer placement={'left'} onClose={onClose} isOpen={isOpen}>
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
-            <DrawerBody>
-              <p>Some contents...</p>
-              <p>Some contents...</p>
-              <p>Some contents...</p>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-      </>
-    )
-  }
+
+  const [isOpen, setisOpen] = useState(false);
+  const onOpen = () => {
+    setisOpen(!isOpen);
+  };
+  const onClose = () => {
+    setisOpen(!isOpen);
+  };
+  return (
+    <>
+      <Button colorScheme="blue" onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer placement={"left"} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Basic Drawer</DrawerHeader>
+          <DrawerBody>
+            <Text>Some contents...</Text>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
