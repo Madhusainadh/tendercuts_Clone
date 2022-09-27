c0580bb105693d4bd55c035eae03ec87912c482e
jstgrowup <deysubham999@gmail.com>
2022-09-27 13:40:45 +0530

    added

--- src/TopPicksForYou/TPFYButton.tsx
+++ src/TopPicksForYou/TPFYButton.tsx
@@ -3,12 +3,15 @@ import React from "react";
 
 function TPFYButton() {
   return (
+    <Box>
+
     <Button bg={"#C11C2D"} _hover={{ backgroundColor: "#C11C2D" }}>
       {" "}
       <Heading size={"sm"} color={"white"}>
         ADD TO CART
       </Heading>{" "}
     </Button>
+    </Box>
   );
 }
 
