9d3cf8fb9fd911659d8b1d7a7530d71b0fd8ec5a
madhusainadh <lalammadhusainadh150@gmail.com>
2022-09-30 23:27:12 +0530

    nothing added

--- src/Itempage/Items.tsx
+++ src/Itempage/Items.tsx
@@ -25,20 +25,21 @@ export const Items = () => {
   }, []);
 
   const Redirectonclickfun =(e:any)=>{
-// console.log(e)
+console.log(e)
   }
-  //   "title": "Dindigul Chicken Biryani Kit",
-  //   "details": "Serves: 2-3 | (Kit Contains:Seeraga Samba Rice 275 g + Chicken Biryani Cut 300 g Dindigul Biryani Paste 200 g)",
-  //   "image": "https://assets.tendercuts.in/product/C/O/e1caaea4-f778-4def-a6cb-73aebab62eeb.jpg",
-  //   "dis": "Do you remember the last time you prepared biryani and how long it took you to do it? It must have taken you somewhere around 2-3 hours, right? Dindigul chicken biryani kit can cut that time in more than half. The chicken pieces are there, the rice is part of the kit and the biryani paste too. It cooks in no time.",
-  //   "weight": "775 Gms",
-  //   "Serves": "2-3",
-  //   "price": 225,
-  //   "mrp": 267,
-  //   "stoke": 3,
-  //   "youtube": "https://youtu.be/x4ELWQRYkiA",
-  //   "type": "Biryani",
-  //   "id": 1
+
+    // "title": "Dindigul Chicken Biryani Kit",
+    // "details": "Serves: 2-3 | (Kit Contains:Seeraga Samba Rice 275 g + Chicken Biryani Cut 300 g Dindigul Biryani Paste 200 g)",
+    // "image": "https://assets.tendercuts.in/product/C/O/e1caaea4-f778-4def-a6cb-73aebab62eeb.jpg",
+    // "dis": "Do you remember the last time you prepared biryani and how long it took you to do it? It must have taken you somewhere around 2-3 hours, right? Dindigul chicken biryani kit can cut that time in more than half. The chicken pieces are there, the rice is part of the kit and the biryani paste too. It cooks in no time.",
+    // "weight": "775 Gms",
+    // "Serves": "2-3",
+    // "price": 225,
+    // "mrp": 267,
+    // "stoke": 3,
+    // "youtube": "https://youtu.be/x4ELWQRYkiA",
+    // "type": "Biryani",
+    // "id": 1
   return (
     <Box maxW={"80%"}>
       <SimpleGrid columns={[1, 1,1, 3]}>
