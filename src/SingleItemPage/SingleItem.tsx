0158369a741794ed971defc6d03fc09fd062f775
madhusainadh <lalammadhusainadh150@gmail.com>
2022-10-07 23:19:48 +0530

    added styling to weight

--- src/SingleItemPage/SingleItem.tsx
+++ src/SingleItemPage/SingleItem.tsx
@@ -1,22 +1,30 @@
-import { Box, Container, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 
 const SingleItem = () => {
   let data = {
-    title: "Dindigul Chicken Biryani Kit",
-    details:
-      "Serves: 2-3 | (Kit Contains:Seeraga Samba Rice 275 g + Chicken Biryani Cut 300 g Dindigul Biryani Paste 200 g)",
+    title: "Premium Goat Curry Cut [1 KG]",
+    details: "35 to 40 piece",
     image:
-      "https://assets.tendercuts.in/product/C/O/e1caaea4-f778-4def-a6cb-73aebab62eeb.jpg",
-    dis: "Do you remember the last time you prepared biryani and how long it took you to do it? It must have taken you somewhere around 2-3 hours, right? Dindigul chicken biryani kit can cut that time in more than half. The chicken pieces are there, the rice is part of the kit and the biryani paste too. It cooks in no time.",
-    weight: "775 Gms",
-    Serves: "2-3",
-    price: 225,
-    mrp: 267,
-    stoke: 3,
-    youtube: "https://youtu.be/x4ELWQRYkiA",
-    type: "Biryani",
-    id: 1,
+      "https://assets.tendercuts.in/product/P/R/44e5fc66-bd7b-437c-aa09-c6873382bd09.webp",
+    dis: "Tender and lean meat from the choicest cuts of goat - the front leg, shoulder and back are expertly cut and trimmed for our goat curry cut. Bursting with delicate earthy flavors and with fall-off-the-bone succulence, our goat curry cut can take you to gastronomic paradise.& Ideal for: Kerala mutton curry, Chettinadu mutton fry, Rogan josh, Rajasthanilalmaas& No of Pieces:35 to 40 & Freshness Indicator: The meat should be moist, brownish-red in color and cold to touch. When you press a finger into the fresh meat, the dentshould quickly disappear and the meat should bounce back to its original shape.",
+    tip: "Tips: Most mutton curries require slow cooking on low heat to retain their moisture, juices and tenderness. The mutton should first be cooked over high heat to seal the juices and then cooked till tender on low heat.",
+    weight: "960 - 980 Gms",
+    Serves: "4 - 6",
+    price: 809,
+    mrp: 839,
+    Pieces: " 35 to 40 piece ",
+    youtube: "",
+    type: "Mutton",
+    id: 21,
   };
   return (
     <div>
@@ -24,26 +32,34 @@ const SingleItem = () => {
         <Heading>{data.title}</Heading>
         <Text>{data.details}</Text>
       </Box>
-      <Container  minW={"1000px"} >
-      <Flex >
-        <Box h={"400px"} w={"400px"}>
-          <Image h={"400px"} w={"400px"} src={data.image} />
-        </Box>
-        <Box>
-          <Text>{data.dis}</Text>
-        </Box>
-        <Flex>
+      <Container maxW={"1000px"}>
+        <SimpleGrid gap={"10px"} columns={[1, 1, 1, 2]}>
+          <Flex justifyContent={"center"}>
+            {" "}
+            <Box>
+              <Image h={"400px"} w={"400px"} src={data.image} />
+            </Box>
+          </Flex>
+
           <Box>
-            <span>Weight : </span>
-            <Text>{data.weight}</Text>
+            <Box>
+              <Text>{data.dis}</Text>
+            </Box>
+            <Flex>
+            <Box display={"flex"} alignItems={"center"} fontSize={"14px"} bg={"#e9e3e3"} p={["6px","10px"]} color={"#414b4e"}  mr={"8px"} h={"32px"} left={"16px"}   >
+               
+                <Text>Weight :{data.weight}</Text>
+              </Box>
+              <Box display={"flex"} alignItems={"center"} fontSize={"14px"} bg={"#e9e3e3"} p={["6px","10px"]} color={"#414b4e"}  mr={"8px"} h={"32px"} left={"16px"}   >
+                <Text>Serves :{data.Serves}</Text>
+              </Box>
+            </Flex>
+            <Flex>
+              {" "}
+              <Text>Price:₹{data.price}</Text>
+            </Flex>
           </Box>
-          <Box>
-            <span>Serves : </span>
-            <Text>{data.Serves}</Text>
-          </Box>
-        </Flex>
-        <Text>{data.price}</Text>
-      </Flex>
+        </SimpleGrid>
       </Container>
     </div>
   );
