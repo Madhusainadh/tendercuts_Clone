5a26cb1bcc4f5ce30f35b018675bd496fc89b09a
jstgrowup <deysubham999@gmail.com>
2022-10-23 22:01:33 +0530

    Folder Modification

--- Frontend/src/Pages/HomePage.tsx
+++ Frontend/src/Pages/HomePage.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, Heading } from "@chakra-ui/react";
 import React from "react";
 import CheckoutOurBlog from "../Home/CheckoutOurBlog/CheckoutOurBlog";
 import TenderCutsAccordion from "../Home/HomePageAccordion/TenderCutsAccordion";
+import HomeSlider from "../Home/HomeSlider/HomeSlider";
 import ShopByMain from "../Home/ShopbyCategory/ShopByMain";
 import TopPickMain from "../Home/TopPicksForYou/TopPickMain";
 import WhyTenderCuts from "../Home/WhyTenderCuts/WhyTenderCuts";
@@ -13,7 +14,7 @@ function HomePage() {
     <>
       <NavBar1 />
       <Navbar2 />
-
+      <HomeSlider />
       <WhyTenderCuts />
       <ShopByMain />
       <TopPickMain />
