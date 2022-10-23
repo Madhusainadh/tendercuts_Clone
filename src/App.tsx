f07514d19b06311d0d867bc57bc61ae121db80f2
jstgrowup <deysubham999@gmail.com>
2022-10-23 09:25:32 +0530

    ready for merger conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 
+import CheckoutOurBlog from "./CheckoutOurBlog/CheckoutOurBlog";
 import { NavBar1 } from "./Component/NavBar1";
 import { Navbar2 } from "./Component/Navbar2";
 import ShopByMain from "./ShopbyCategory/ShopByMain";
@@ -8,10 +9,7 @@ import TopPickMain from "./TopPicksForYou/TopPickMain";
 function App() {
   return (
     <div className="App">
-      <NavBar1 />
-      <Navbar2 />
-      <ShopByMain />
-      <TopPickMain/>
+     
     </div>
   );
 }
